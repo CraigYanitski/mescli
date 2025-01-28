566f42ede2188510d8c30415ebb697d0cb3c2826
yanitski <yanitski@ph1.uni-koeln.de>
2025-01-27 19:53:18 -0700

    new function to generate private key

--- cryptography/crytography.go
+++ cryptography/crytography.go
@@ -1,6 +1,8 @@
 package cryptography
 
 import (
+    "crypto/ecdh"
+    "crypto/rand"
     "fmt"
     "golang.org/x/crypto/bcrypt"
 )
@@ -29,5 +31,16 @@ func generateSalt(length int) ([]byte, error) {
 		return nil, err
 	}
 	return salt[:], nil
+}
+
+func generateECDH() (*ecdh.PrivateKey, error) {
+    // Generate private key
+    key, err := ecdh.P256().GenerateKey(rand.Reader)
+
+    // Return error if failed, else return key
+    if err != nil {
+        return nil, fmt.Errorf("error generating private key: %v", err)
+    }
+    return key, nil
 }
 
