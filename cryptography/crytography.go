aa005cb62a73860ed0b3881788d93274878cbeb7
yanitski <yanitski@ph1.uni-koeln.de>
2025-01-28 14:50:17 -0700

    made some functions public and commented code

--- cryptography/crytography.go
+++ cryptography/crytography.go
@@ -7,17 +7,23 @@ import (
     "golang.org/x/crypto/bcrypt"
 )
 
-func hashPassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), 13)
+func HashPassword(password string) (string, error) {
+	// Hash password a specified number of times
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 13)
+
+    // return error if failed, else return hash as a string
 	if err != nil {
 		return "", err
 	}
 	return string(hash), nil
 }
 
-func checkPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	if err != nil {
+func CheckPasswordHash(password, hash string) bool {
+	// Compare password to hash
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	
+    // Print error if failed and return false, else return true
+    if err != nil {
 		fmt.Println(err)
 		return false
 	}
@@ -25,15 +31,20 @@ func checkPasswordHash(password, hash string) bool {
 }
 
 func generateSalt(length int) ([]byte, error) {
+    // Initialise salt slice of specified length
 	salt := make([]byte, length)
+
+    // Generate random salt
 	l, err := rand.Read(salt)
+
+    // Return error if failed, else return salt
 	if (l != length) || (err != nil) {
 		return nil, err
 	}
 	return salt[:], nil
 }
 
-func generateECDH() (*ecdh.PrivateKey, error) {
+func GenerateECDH() (*ecdh.PrivateKey, error) {
     // Generate private key
     key, err := ecdh.P256().GenerateKey(rand.Reader)
 
