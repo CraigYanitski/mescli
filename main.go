c2429daa065de294c7a9d83f005108b5073a0506
yanitski <yanitski@ph1.uni-koeln.de>
2025-02-03 10:51:51 -0700

    removed old code and updated tests

--- main.go
+++ main.go
@@ -8,21 +8,10 @@ import (
 )
 
 func main() {
-    /*
-    // Initialise ANSI codes for test
-    var codes = []typeset.AnsiCMD{3, 31}
-    var reset = []typeset.AnsiCMD{0}
-
-    // Get ANSI strings
-    prefix, err := typeset.FormatANSI(codes)
-    if err != nil {
-        panic(err)
-    }
-    suffix, err := typeset.FormatANSI(reset)
-    if err != nil {
-        panic(err)
-    }
-    */
+    // Test prelude
+    fmt.Printf("\n---------------\n")
+    fmt.Println("typesetting test")
+    fmt.Printf("---------------\n\n")
 
     // Test string
     line := "A test string!!"
@@ -39,134 +28,10 @@ func main() {
     // Test printing to terminal
     fmt.Println(formattedLine)
 
-    /*
-    // Separate tests
-    fmt.Printf("\n---------------\n\n")
-
-    // Create client 1
-    alice := client.Client{Name: "Alice"}
-    
-    err = alice.HashPassword("Alice's very secure password")
-    if err != nil {
-        err = fmt.Errorf("Cannot hash Alice's password!! : %v", err)
-        panic(err)
-    }
-
-    err = alice.GenerateKey()
-    if err != nil {
-        err = fmt.Errorf("Cannot generate Alice's key : %v", err)
-        panic(err)
-    }
-
-    alicePrivKey := alice.KeyDH.Bytes()
-    fmt.Printf("\nAlice's private key: %x\n", alicePrivKey)
-    alicePubKey := alice.KeyDH.PublicKey()
-    fmt.Printf("Alice's public key: %x\n", alicePubKey.Bytes())
-
-    // Create client 2
-    bob := client.Client{Name: "Bob"}
-    
-    err = bob.HashPassword("Bob's equally secure password")
-    if err != nil {
-        err = fmt.Errorf("Cannot hash Bob's password!! : %v", err)
-        panic(err)
-    }
-
-    err = bob.GenerateKey()
-    if err != nil {
-        err = fmt.Errorf("Cannot generate Bob's key : %v", err)
-        panic(err)
-    }
-
-    bobPrivKey := bob.KeyDH.Bytes()
-    fmt.Printf("\nBob's private key: %x\n", bobPrivKey)
-    bobPubKey := bob.KeyDH.PublicKey()
-    fmt.Printf("Bob's public key: %x\n", bobPubKey.Bytes())
-
-    // Calculate both clients' secrets
-    //Alice
-    aliceSecret, err := alice.KeyDH.ECDH(bobPubKey)
-
-    if err != nil {
-        err = fmt.Errorf("Error getting Alice's secret : %v", err)
-        panic(err)
-    }
-
-    fmt.Printf("\nAlice's secret (%d): %x\n", len(aliceSecret), aliceSecret)
-
-    // Bob
-    bobSecret, err := bob.KeyDH.ECDH(alicePubKey)
-
-    if err != nil {
-        err = fmt.Errorf("Error getting Bob's secret : %v", err)
-        panic(err)
-    }
-    fmt.Printf("Bob's secret (%d): %x\n", len(bobSecret), bobSecret)
-
-    // Confirm whether or not they are equal, and thus the exchange is complete
-    if !bytes.Equal(aliceSecret, bobSecret) {
-        err = fmt.Errorf("Error in establishing DH exchange!! : %v", err)
-        panic(err)
-    }
-    result, _ := typeset.FormatString("\nDiffie-Hellman exchange complete", []string{"italics", "green"})
-    fmt.Println(result)
-
-    // Renew keys in separate test
-    fmt.Printf("\n---------------\n\n")
-
-    err = alice.GenerateKey()
-    if err != nil {
-        err = fmt.Errorf("Cannot generate Alice's key : %v", err)
-        panic(err)
-    }
-
-    alicePrivKey = alice.KeyDH.Bytes()
-    fmt.Printf("\nAlice's private key: %x\n", alicePrivKey)
-    alicePubKey = alice.KeyDH.PublicKey()
-    fmt.Printf("Alice's public key: %x\n", alicePubKey.Bytes())
-
-    err = bob.GenerateKey()
-    if err != nil {
-        err = fmt.Errorf("Cannot generate Bob's key : %v", err)
-        panic(err)
-    }
-
-    bobPrivKey = bob.KeyDH.Bytes()
-    fmt.Printf("\nBob's private key: %x\n", bobPrivKey)
-    bobPubKey = bob.KeyDH.PublicKey()
-    fmt.Printf("Bob's public key: %x\n", bobPubKey.Bytes())
-
-    // Calculate both clients' secrets
-    //Alice
-    aliceSecret, err = alice.KeyDH.ECDH(bobPubKey)
-
-    if err != nil {
-        err = fmt.Errorf("Error getting Alice's secret : %v", err)
-        panic(err)
-    }
-
-    fmt.Printf("\nAlice's secret (%d): %x\n", len(aliceSecret), aliceSecret)
-
-    // Bob
-    bobSecret, err = bob.KeyDH.ECDH(alicePubKey)
-
-    if err != nil {
-        err = fmt.Errorf("Error getting Bob's secret : %v", err)
-        panic(err)
-    }
-    fmt.Printf("Bob's secret (%d): %x\n", len(bobSecret), bobSecret)
-
-    // Confirm whether or not they are equal, and thus the exchange is complete
-    if !bytes.Equal(aliceSecret, bobSecret) {
-        err = fmt.Errorf("Error in establishing DH exchange!! : %v", err)
-        panic(err)
-    }
-    result, _ = typeset.FormatString("\nDiffie-Hellman exchange complete", []string{"italics", "green"})
-    fmt.Println(result)
-    */
-    
-    // Separate tests
-    fmt.Printf("\n---------------\n\n")
+    // Test prelude
+    fmt.Printf("\n---------------\n")
+    fmt.Println("X3DH test")
+    fmt.Printf("---------------\n\n")
 
     // Initialise clients in conversation
     alice := client.Client{Name: "Alice"}
@@ -176,19 +41,19 @@ func main() {
 
     // Perform extended triple Diffie-Hellman exchange
     alice.EstablishX3DH(bob)
-    fmt.Printf("\n%v secret key: %x\n", alice.Name, alice.Secret)
+    fmt.Printf("%v's secret key: %x\n", alice.Name, alice.Secret)
     bob.CompleteX3DH(alice)
-    fmt.Printf("%v secret key: %x\n", bob.Name, bob.Secret)
+    fmt.Printf("%v's secret key: %x\n", bob.Name, bob.Secret)
 
     // Check if exchange was successful
     // Confirm whether or not they are equal, and thus the exchange is complete
+    var result string
     if !bytes.Equal(alice.Secret, bob.Secret) {
-        err, _ := typeset.FormatString("\nError in establishing X3DH exchange! Secrets are not equal!!", 
+        result, _ = typeset.FormatString("\nError in establishing X3DH exchange! Secrets are not equal!!", 
             []string{"italics", "red"})
-        // fmt.Println(err)
-        panic(err)
+    } else {
+        result, _ = typeset.FormatString("\nExtended triple Diffie-Hellman exchange complete", 
+            []string{"italics", "green"})
     }
-    result, _ := typeset.FormatString("\nExtended triple Diffie-Hellman exchange complete", 
-        []string{"italics", "green"})
     fmt.Println(result)
 }
