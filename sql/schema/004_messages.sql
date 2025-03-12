d25bb302509f011386ed15940540d34b95ab6c50
yanitski <yanitski@ph1.uni-koeln.de>
2025-03-11 23:35:53 -0600

    new sender_id field

--- sql/schema/004_messages.sql
+++ sql/schema/004_messages.sql
@@ -4,6 +4,7 @@ CREATE TABLE messages (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
+    sender_id UUID NOT NULL REFERENCES users,
     message TEXT NOT NULL
 ) ;
 
