78bfec657dcc6f4a60b0ab639a200370315dec05
yanitski <yanitski@ph1.uni-koeln.de>
2025-03-27 20:14:02 -0600

    implemented handler to add contact
    * include key packet in message struct to simplify messaging
    * decrypt messages pulled from server

--- app/messages.go
+++ app/messages.go
@@ -24,21 +24,66 @@ type UserKeyPacket struct {
     OnetimePrekey  string     `json:"onetime_prekey"`
 }
 type MessageRequest struct {
-    UserID    uuid.UUID  `json:"user_id"`
-    SenderID  uuid.UUID  `json:"sender_id"`
-    Message   string     `json:"message"`
+    UserID              uuid.UUID  `json:"user_id"`
+    SenderID            uuid.UUID  `json:"sender_id"`
+    Message             string     `json:"message"`
+    SenderIdentityKey   string     `json:"sender_identity_key"`
+    SenderEphemeralKey  string     `json:"sender_ephemeral_key"`
 }
 type MessageResponse struct {
-    ID         uuid.UUID  `json:"id"`
-    CreatedAt  time.Time  `json:"created_at"`
-    UpdatedAt  time.Time  `json:"updated_at"`
-    UserID    uuid.UUID  `json:"user_id"`
-    SenderID  uuid.UUID  `json:"sender_id"`
-    Message   string     `json:"message"`
+    ID                  uuid.UUID  `json:"id"`
+    CreatedAt           time.Time  `json:"created_at"`
+    UpdatedAt           time.Time  `json:"updated_at"`
+    UserID              uuid.UUID  `json:"user_id"`
+    SenderID            uuid.UUID  `json:"sender_id"`
+    Message             string     `json:"message"`
+    SenderIdentityKey   string     `json:"sender_identity_key"`
+    SenderEphemeralKey  string     `json:"sender_ephemeral_key"`
 }
 
-func addContact(email string) error {
-    return nil
+func addContact(email string) (*client.MessagePacketJSON, error) {
+    apiURL := viper.GetString("api_url")
+    httpClient := http.Client{}
+    u := client.Client{}
+    u.Initialise(false)
+    // get contact key packet
+    senderID, err := getContact(email)
+    if err != nil {
+        return nil, err
+    }
+    keyReqStruct := UserResponse{ID: *senderID}
+    data, err := json.Marshal(keyReqStruct)
+    if err != nil {
+        return nil, err
+    }
+    // send request to server
+    keyReq, err := http.NewRequest(http.MethodGet, apiURL+"/users", bytes.NewBuffer(data))
+    if err != nil {
+        return nil, err
+    }
+    keyReq.Header.Set("Content-Type", "application/json")
+    keyReq.Header.Set("Authorization", "Bearer "+viper.GetString("access_token"))
+    keyResp, err := httpClient.Do(keyReq)
+    if err != nil {
+        return nil, err
+    }
+    senderKeys := &UserKeyPacket{}
+    keyRespData, err := io.ReadAll(keyResp.Body)
+    if err != nil {
+        return nil, err
+    }
+    err = json.Unmarshal(keyRespData, senderKeys)
+    if err != nil {
+        return nil, err
+    }
+    senderKeyPacket := &client.PrekeyPacketJSON{
+        IdentityKey: senderKeys.IdentityKey,
+        SignedPrekey: senderKeys.SignedPrekey,
+        SignedKey: senderKeys.SignedKey,
+        OnetimePrekey: senderKeys.OnetimePrekey,
+    }
+    messageJSON := u.InitiateX3DH(senderKeyPacket, *senderID, false)
+    return messageJSON, nil
 }
 
 func getUserIdentityKey(user uuid.UUID) (*ecdh.PublicKey, error) {
@@ -74,24 +119,34 @@ func getUserIdentityKey(user uuid.UUID) (*ecdh.PublicKey, error) {
     return userIK, nil
 }
 
-func sendMessage(user uuid.UUID, message string) error {
+func sendMessage(contactID uuid.UUID, contactX3DHpacket *client.MessagePacketJSON, message string) error {
     apiURL := viper.GetString("api_url")
     httpClient := http.Client{}
     c := client.Client{}
     c.Initialise(false)
     // get contact identity key
-    contactIK, err := getUserIdentityKey(user)
+    contactIK, err := getUserIdentityKey(contactID)  // TODO: change input to string (and function to decode string)
     if err != nil {
         return fmt.Errorf("error getting contact identity key: %s", err)
     }
     // encrypt message and marshal request JSON
-    encryptedMsg, err := c.SendMessage(message, []string{}, contactIK, false)
+    encryptedMsg, err := c.SendMessage(message, []string{}, contactIK, contactID, false)
     if err != nil {
         return err
     }
+    // check messagePacketJSON
+    if contactX3DHpacket == nil {
+        contactX3DHpacket = &client.MessagePacketJSON{
+            IdentityKey: cryptography.EncodeECDSAPublicKey(contactIK),
+            EphemeralKey: "",
+        }
+    }
+    // marshal request JSON
     msg := MessageRequest{
-        UserID: user,
+        UserID: contactID,
         Message: encryptedMsg,
+        SenderIdentityKey: contactX3DHpacket.IdentityKey,
+        SenderEphemeralKey: contactX3DHpacket.EphemeralKey,
     }
     msgData, err := json.Marshal(msg)
     if err != nil {
@@ -148,8 +203,49 @@ func getMessages() (messages []MessageResponse, err error) {
         return
     }
     for _, message := range *messagesSlice {
+        // get sender conversation from config
+        senderMessages := viper.GetStringSlice("contacts."+message.SenderID.String()+".messages")
+        senderEncryptedMessages := viper.GetStringSlice("contacts."+message.SenderID.String()+".encrypted_messages")
+        senderEncryptedMessages = append(senderEncryptedMessages, message.Message)
+        viper.Set("contacts."+message.SenderID.String()+".encrypted_messages", senderEncryptedMessages)
+        // check if X3DH initiated
+        if message.SenderEphemeralKey != "" {
+            err = c.CompleteX3DH(
+                &client.MessagePacketJSON{
+                    IdentityKey: message.SenderIdentityKey,
+                    EphemeralKey: message.SenderEphemeralKey,
+                },
+                message.SenderID,
+                false,
+            )
+            if err != nil {
+                senderMessages = append(senderMessages, message.Message)
+                viper.Set("contacts."+message.SenderID.String()+".messages", senderMessages)
+                continue
+            }
+        }
+        // get the sender identity key
+        senderIK, err := getUserIdentityKey(message.SenderID)
+        if err != nil {
+            senderMessages = append(senderMessages, message.Message)
+            viper.Set("contacts."+message.SenderID.String()+".messages", senderMessages)
+            continue
+        }
+        decryptedMessage, err := c.ReceiveMessage(
+            message.Message, 
+            senderIK, 
+            message.SenderID, 
+            false)
+        if err != nil {
+            senderMessages = append(senderMessages, message.Message)
+            viper.Set("contacts."+message.SenderID.String()+".messages", senderMessages)
+            continue
+        }
+        senderMessages = append(senderMessages, decryptedMessage)
+        viper.Set("contacts."+message.SenderID.String()+".messages", senderMessages)
         messages = append(messages, message)
     }
+    viper.WriteConfig()
     return
 }
 
